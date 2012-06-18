2c3e964dd5c962224e2f1b326d4fcce40958a75c
Moises Vega <moysess@gmail.com>
2012-06-18 01:06:35 -0500

    Add python/install.sh

---
+++ python/install.sh
@@ -0,0 +1,32 @@
+# This script is going to install all you need for your
+# python development
+
+# Get python first
+apt-get install python
+
+# Install python-setuptools
+apt-get install python-setuptools python-dev build-essential git-core -y
+
+# Install pip
+easy_install pip
+
+# Install virtualenv
+# Info : http://pypi.python.org/pypi/virtualenv
+pip install virtualenv
+
+# Install virtualenvwrapper
+# Info : http://pypi.python.org/pypi/virtualenvwrapper
+pip install virtualenvwrapper
+
+# Install pythonbrew
+# Info : http://pypi.python.org/pypi/pythonbrew/
+pip install pythonbrew
+
+# Install django
+# Info : https://www.djangoproject.com/
+pip install Django
+
+# IPython
+# Info : http://ipython.org/
+pip install ipython
+
